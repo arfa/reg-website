21a630ecc560636c946fd29703c8cfeab68fdd5c
Marwen Arfa <marwen.arfa@gmail.com>
2024-03-24 21:16:02 +0100

    feat: add analytics

--- app/(personal)/layout.tsx
+++ app/(personal)/layout.tsx
@@ -1,4 +1,5 @@
 import { toPlainText } from '@portabletext/react'
+import { Analytics } from '@vercel/analytics/react'
 import { Metadata, Viewport } from 'next'
 import dynamic from 'next/dynamic'
 import { draftMode } from 'next/headers'
@@ -65,6 +66,7 @@ export default async function IndexRoute({
         </Suspense>
         <Toaster />
         <ScrollToTop />
+        <Analytics />
       </div>
       {draftMode().isEnabled && <VisualEditing />}
     </ThemeProvider>
